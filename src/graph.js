e04dc140cae213749e152ea0f845234cdc381c90
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-06-11 00:53:22 +0530

    Added getAdjTable method to find the adjacent nodes

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,21 @@
-//Example 
+//Example
 // Pairs => [[from,to],[to,from]]
 // Source => from
-// To => to 
+// To => to
 // Should return true.
 
-const bfs = function(pairs,source,target){
-  
+const getAdjTable = function (pairs) {
+  const adjTable = {};
+  pairs.forEach((pair) => {
+    if (pair[0] in adjTable) {
+      adjTable[pair[0]].push(pair[1]);
+    } else {
+      adjTable[pair[0]] = [pair[1]];
+    }
+  });
+  return adjTable;
 };
 
-module.exports = {bfs};
+const bfs = function (pairs, source, target) {};
+
+module.exports = { bfs };
